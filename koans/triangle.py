023e702dd60baa3dab6d9fa7272d1967ebd7fe1b
Nick <elanir358@gmail.com>
2025-04-13 12:53:31 +0300

    triangle complete

--- koans/triangle.py
+++ koans/triangle.py
@@ -16,9 +16,24 @@
 # and
 #   about_triangle_project_2.py
 #
-def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+# def triangle(a, b, c):
+#     # DELETE 'PASS' AND WRITE THIS CODE
+#     # if a == b and b == c:
+#     #     return 'equilateral'
+#     # elif a == b or b == c or a == c:
+#     #     return 'isosceles'
+#     # else:
+#     #     return 'scalene'
+#     if a == b == c:
+#         return 'equilateral'
+#     elif a == b or b == c or a == c:
+#         return 'isosceles'
+#     else:
+#         return 'scalene'
+
+
+triangle = lambda a, b, c: 'equilateral' if a == b == c else 'isosceles' if a == b or b == c or a == c else 'scalene'
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
